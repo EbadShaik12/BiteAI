05cce87d5a1efa77b2bfb2c985368544eaeade61
BiteAI Pilot <pilot@biteai.corp>
2026-05-20 16:40:30 +0530

    fix(frontend): integrate centralized Axios request interceptor for automated authorization Bearer tokens

---
+++ frontend/src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+import './index.css';
+import axios from 'axios';
+
+// Add a request interceptor to attach JWT token to all outbound API calls
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('biteai_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
